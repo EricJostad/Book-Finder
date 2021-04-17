c46a93d211a318cb5dfe5710d3638e3ea038a19d
EJostad <eric.jostad@outlook.com>
2021-04-16 23:15:58 -0700

    Updated App.js file.

--- book-finder/src/utils/api.js
+++ book-finder/src/utils/api.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  searchBooks: (query) => {
-      return axios.get('https://www.googleapis.com/books/v1/volumes?q='+ query)
-  },
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+    searchBooks: (query) => {
+        return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + query)
+    },
+    getBooks: function () {
+        return axios.get("/api/books");
+    },
+    getBook: function (id) {
+        return axios.get("/api/books/" + id);
+    },
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id);
+    },
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData);
+    }
 };
