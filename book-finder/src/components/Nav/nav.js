e6825e23bf237a38e852a0f5d4ffe6fbfc9567a0
EJostad <eric.jostad@outlook.com>
2021-04-17 00:04:51 -0700

    Small change to Nav component. Set up finder component.

--- book-finder/src/components/Nav/nav.js
+++ book-finder/src/components/Nav/nav.js
@@ -3,20 +3,20 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
 const Navigator = () => {
-  
-  return (
-    <Navbar bg='dark' variant='dark'>
-      <Navbar.Brand>Book Finder</Navbar.Brand>
-        <Nav className='mr-auto'>
-        <Nav.Item>
-          <Nav.Link as={Link} to='/'>Search</Nav.Link>
-        </Nav.Item>
-        <Nav.Item to='/more'>
-          <Nav.Link as={Link} to='/saved'>Saved</Nav.Link>
-        </Nav.Item>
-        </Nav>
-    </Navbar>
-  );
+
+    return (
+        <Navbar bg='dark' variant='dark'>
+            <Navbar.Brand>Book Finder</Navbar.Brand>
+            <Nav className='mr-auto'>
+                <Nav.Item>
+                    <Nav.Link as={Link} to='/'>Search</Nav.Link>
+                </Nav.Item>
+                <Nav.Item to='/more'>
+                    <Nav.Link as={Link} to='/saved'>Saved</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Navbar>
+    );
 }
 
 export default Navigator;
